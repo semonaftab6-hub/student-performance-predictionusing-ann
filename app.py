f5ce6e6ecb32e5b7ec32f1a506b0e927a9e3b834
Semon Aftab <tumharaemail@gmail.com>
2025-11-12 14:39:15 +0530

    modified today

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask ,render_template
-app=Flask(__name__)
+from flask import Flask, render_template
+
+# Create Flask app
+app = Flask(__name__)
+
+# ---------------- ROUTES ---------------- #
+
 @app.route('/')
 def home():
+    # Default page: Login page
+    return render_template('login.html')
+
+@app.route('/index')
+def index():
     return render_template('index.html')
-if __name__=='__main__' : # type: ignore
-    app.run(host='0.0.0.0', port=5000)
+
+@app.route('/train')
+def train():
+    return render_template('train.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+
+# ---------------------------------------- #
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
